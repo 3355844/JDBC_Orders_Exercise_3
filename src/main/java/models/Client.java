55a5ad39e0e847ee3721f99219ad1dcaf5c3864e
3355844 <3355844@gmail.com>
2017-02-04 12:05:01 +0200

    Client Goods Order

---
+++ src/main/java/models/Client.java
@@ -0,0 +1,53 @@
+package models;
+
+/**
+ * Created by 33558 on 04.02.2017.
+ */
+public class Client {
+    private int idClient;
+    private String nameClient;
+    private String addressClient;
+    private String phoneClient;
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public String getNameClient() {
+        return nameClient;
+    }
+
+    public void setNameClient(String nameClient) {
+        this.nameClient = nameClient;
+    }
+
+    public String getAddressClient() {
+        return addressClient;
+    }
+
+    public void setAddressClient(String addressClient) {
+        this.addressClient = addressClient;
+    }
+
+    public String getPhoneClient() {
+        return phoneClient;
+    }
+
+    public void setPhoneClient(String phoneClient) {
+        this.phoneClient = phoneClient;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "idClient=" + idClient +
+                ", nameClient='" + nameClient + '\'' +
+                ", addressClient='" + addressClient + '\'' +
+                ", phoneClient='" + phoneClient + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/models/Order.java
@@ -0,0 +1,34 @@
+package models;
+
+/**
+ * Created by 33558 on 04.02.2017.
+ */
+public class Order {
+    private int idOrder;
+    private int idClient;
+    private int idGoods;
+
+    public int getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(int idOrder) {
+        this.idOrder = idOrder;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public int getIdGoods() {
+        return idGoods;
+    }
+
+    public void setIdGoods(int idGoods) {
+        this.idGoods = idGoods;
+    }
+}
