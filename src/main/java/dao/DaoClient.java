b6b9722cee375ab7a32ee82a920f413194494a94
3355844 <3355844@gmail.com>
2017-02-04 12:55:04 +0200

    Client DAO methods add and get by id

--- src/main/java/dao/DaoClient.java
+++ src/main/java/dao/DaoClient.java
@@ -4,6 +4,7 @@ import models.Client;
 import utills.MySqlConnector;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -17,13 +18,34 @@ public class DaoClient {
         connector = new MySqlConnector();
     }
 
-    public void addClient(Client client) {
+    public void addClientToDB(Client client) {
         PreparedStatement statement;
-        String query = "INSERT INTO apartments (REGION, ADDRESS, SQUARE, ROOMS, PRICE) VALUES (?,?,?,?,?)";
+        String query = "INSERT INTO clients (NAME_CLIENT, ADDRESS_CLIENT, PHONE_CLIENT) VALUES (?,?,?)";
         try {
             statement = connector.getConnection().prepareStatement(query);
+            statement.setString(1, client.getNameClient());
+            statement.setString(2, client.getAddressClient());
+            statement.setString(3, client.getPhoneClient());
+            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public Client getClientByID(int id) {
+        String query = "SELECT * FROM clients WHERE ID_CLIENT ='" + id + "';";
+        Client client = new Client();
+        try {
+            ResultSet resultSet = connector.getConnection().createStatement().executeQuery(query);
+            while (resultSet.next()) {
+                client.setIdClient(resultSet.getInt("ID_CLIENT"));
+                client.setNameClient(resultSet.getString("NAME_CLIENT"));
+                client.setAddressClient(resultSet.getString("ADDRESS_CLIENT"));
+                client.setPhoneClient(resultSet.getString("PHONE_CLIENT"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return client;
     }
 }
